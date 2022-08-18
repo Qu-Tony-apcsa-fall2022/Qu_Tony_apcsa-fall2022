3fd416e5b65bf77331eac690161760c4175d420c
Qu-Tony-apcsa-fall2022 <111453382+Qu-Tony-apcsa-fall2022@users.noreply.github.com>
2022-08-18 11:03:14 -0700

    Add files via upload

---
+++ Unit2Assignments/Line.java
@@ -0,0 +1,14 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+//Date -
+package Unit2;
+public class Line
+{
+	public static double getSlope( int x1, int y1, int x2, int y2 )
+	{
+		float result = (float)(y2-y1)/(x2-x1);
+		return (result);
+	}
+
+}
---
+++ Unit2Assignments/Trap.java
@@ -0,0 +1,14 @@
+//� A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  - 
+package Unit2;
+public class Trap
+{
+	public static double area( int base1, int base2, int height )
+	{
+		float result = (float)(base1+base2)  * (float)height / 2;
+		return result;
+	}
+}
