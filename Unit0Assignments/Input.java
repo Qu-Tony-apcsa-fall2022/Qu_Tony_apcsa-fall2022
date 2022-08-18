bc9b5e03c92c89ae3814c30ac7743df692944722
Qu-Tony-apcsa-fall2022 <111453382+Qu-Tony-apcsa-fall2022@users.noreply.github.com>
2022-08-18 11:07:35 -0700

    Update Input.java

--- Unit0Assignments/Input.java
+++ Unit0Assignments/Input.java
@@ -1,10 +1,10 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
 
-//Name -
-//Date -
-//Class -
-//Lab  -
+//Name - Tony Qu	
+//Date - 8/17/2022
+//Class - APCSA
+//Lab  - Input
 
 import static java.lang.System.*;
 import java.util.Scanner;
