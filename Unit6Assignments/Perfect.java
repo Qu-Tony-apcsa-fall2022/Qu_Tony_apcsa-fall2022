04efcb3813168621fbdc9947009adfcf8b3967af
Qu-Tony-apcsa-fall2022 <111453382+Qu-Tony-apcsa-fall2022@users.noreply.github.com>
2022-08-26 10:55:29 -0700

    Add files via upload

--- Unit6Assignments/Perfect.java
+++ Unit6Assignments/Perfect.java
@@ -1,22 +1,53 @@
+package Unit6;
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
-
+//Name - Tony Qu
+//Date - 08/23/2022
+//Class - APCSA
 import static java.lang.System.*;
-
+import java.lang.Math;
+import java.util.ArrayList;
 public class Perfect
 {
-   private int number;
+	private int number, sum;
+	private ArrayList<Integer> factors;
 
 	//add constructors
-
+   	public Perfect() {
+   		
+   	}
+   	public Perfect(int num) {
+   		setNums(num);
+   	}
 	//add a set method
-
+   	public void setNums(int num) {
+   		number = num;
+   	}
+   	public void setFactors() {
+   		factors = getFactors();
+   		for (int i = 0; i < factors.size(); i ++) {
+   			sum += factors.get(i);
+   		}
+   	}
+   	public ArrayList<Integer> getFactors() {
+   		ArrayList<Integer> f = new ArrayList<Integer>();
+   		int largestfactor = number - 1;
+   		for (int i = largestfactor; i > 0; i --) {
+   			if(number % i == 0) {
+   				f.add(i);
+   			}
+   		}
+   		return f;
+   		
+   	}
 	public boolean isPerfect()
 	{
-		return false;
+		setFactors();
+		return sum == number;
 	}
 
 	//add a toString	
-	
+	public String toString() {
+		return isPerfect() ? number + " is perfect": number + " is not perfect";
+	}
 }
--- Unit6Assignments/PerfectRunner.java
+++ Unit6Assignments/PerfectRunner.java
@@ -1,14 +1,19 @@
+package Unit6;
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
-
+//Name - Tony Qu
+//Date - 08/23/2022
+//Class - APCSA
 import static java.lang.System.*; 
-
+import java.util.Scanner;
 public class PerfectRunner
 {
 	public static void main( String args[] )
 	{
 		//add test cases
-																
+		Scanner keyboard = new Scanner(System.in);
+		out.print("Enter an integer :: ");
+		Perfect detector = new Perfect(keyboard.nextInt());
+		out.print(detector);
 	}
 }
